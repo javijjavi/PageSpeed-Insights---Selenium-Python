87448f148b1fdcf8154469fc5b83cfb89dbf6deb
javijjavi <javigarcia118@gmail.com>
2019-06-06 14:59:42 +0200

    work

--- miservidor/pagespeed/pagespeed/settings.py
+++ miservidor/pagespeed/pagespeed/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u728nno4339(0#)x_tyc*im+ack=2f-v1a&=6l%27dlkzlhp!m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.50.182']
 
 
 # Application definition
